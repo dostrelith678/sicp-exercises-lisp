a42f4e88900c0da6ea051757ae522dc5e94094fb
dostrelith <damjan.ostrelic@gmail.com>
2023-09-05 21:13:44 +0100

    Ex. 1.9 - 1.10

---
+++ Chapter1/1.10.rkt
@@ -0,0 +1,137 @@
+#lang racket
+
+#|
+  Exercise 1.10: The following procedure computes a mathematical
+  function called Ackermann’s function.
+
+  (define (A x y)
+    (cond ((= y 0) 0)
+          ((= x 0) (* 2 y))
+          ((= y 1) 2)
+          (else (A (- x 1) (A x (- y 1))))))
+
+  What are the values of the following expressions?
+  (A 1 10)
+  (A 2 4)
+  (A 3 3)
+
+  Consider the following procedures, where A is the procedure
+  defined above:
+  (define (f n) (A 0 n))
+  (define (g n) (A 1 n))
+  (define (h n) (A 2 n))
+  (define (k n) (* 5 n n))
+  Give concise mathematical definitions for the functions computed
+  by the procedures f, g, and h for positive integer values
+  of n. For example, (k n) computes 5n^2.
+|#
+
+
+#|
+  (A 1 10)
+  (A (- 1 1) (A 1 (- 10 1)))
+  (A 0 (A 1 9))
+  (A 0 (A (- 1 1) (A 1 8)))
+  (A 0 (A 0 (A (- 1 1) (A 1 7))))
+  (A 0 (A 0 (A 0 (A (- 1 1) (A 1 6)))))
+  (A 0 (A 0 (A 0 (A 0 (A (- 1 1) (A 1 5))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A (- 1 1) (A 1 4)))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A (- 1 1) (A 1 3)))))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A (- 1 1) (A 1 2))))))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A (- 1 1) (A 1 1)))))))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2))))))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8))))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16)))))
+  (A 0 (A 0 (A 0 (A 0 (A 0 32))))
+  (A 0 (A 0 (A 0 (A 0 64)))
+  (A 0 (A 0 (A 0 128))
+  (A 0 (A 0 256)
+  (A 0 512)
+  1024
+
+  (A 2 4)
+  (A 1 (A 2 3))
+  (A 1 (A 1 (A 2 2)))
+  (A 1 (A 1 (A 1 (A 2 1))))
+  (A 1 (A 1 (A 1 2)))
+  (A 1 (A 1 (A 0 (A 1 1))))
+  (A 1 (A 1 (A 0 2)))
+  (A 1 (A 1 4))
+  (A 1 (A 0 (A 1 3)))
+  (A 1 (A 0 (A 0 (A 1 2))))
+  (A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+  (A 1 (A 0 (A 0 (A 0 2))))
+  (A 1 (A 0 (A 0 4)))
+  (A 1 (A 0 8))
+  (A 1 16)
+  ...
+
+
+  (A 3 3)
+  ...
+
+
+  (define (f n) (A 0 n))
+  (define (g n) (A 1 n))
+  (define (h n) (A 2 n))
+
+  (f 1)
+  (A 0 1)
+  2
+  (f 2)
+  (A 0 2)
+  4
+
+  (f n) = n * 2
+
+  (g 1)
+  (A 1 1)
+  2
+
+  (g 2)
+  (A 1 2)
+  (A 0 (A 1 1))
+  (A 0 2)
+  4
+
+  (g 3)
+  (A 1 3)
+  (A 0 (A 1 2))
+  (A 0 (A 0 (A 1 1)))
+  (A 0 (A 0 2))
+  (A 0 4)
+  8
+
+  (g n) = 2 ^ n
+
+  (h 1)
+  (A 2 1)
+  2
+
+  (h 2)
+  (A 2 2)
+  (A 1 (A 2 1))
+  (A 1 2)
+  (A 0 (A 1 1))
+  (A 0 2)
+  4
+
+  (h 3)
+  (A 2 3)
+  (A 1 (A 2 2))
+  (A 1 (A 1 (A 2 1)))
+  (A 1 (A 1 2))
+  (A 1 (A 0 (A 1 1)))
+  (A 1 (A 0 2))
+  (A 1 4)
+  (A 0 (A 1 3))
+  (A 0 (A 0 (A 1 2)))
+  (A 0 (A 0 (A 0 (A 1 1))))
+  (A 0 (A 0 (A 0 2)))
+  (A 0 (A 0 4))
+  (A 0 8)
+  16
+
+  (h n) = 2 ^ (h (- n 1))
+|#
